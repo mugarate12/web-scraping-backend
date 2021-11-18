2a53be7acefbb8a764bca07b58b56d31a24a8554
Mateus Cardoso <serjumano17@gmail.com>
2021-11-18 01:53:47 -0300

    feat!: added docs to public access routes

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,26 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import http from 'http'
+import { errors } from 'celebrate'
 import { Server } from 'socket.io'
+import swaggerUI from 'swagger-ui-express'
 
 import routes from './routes'
 import routines from './routines'
 import RunSockets from './sockets'
 
+let publicAccessDocuments
+
 dotenv.config()
 const app = express()
 const server = http.createServer(app)
+
+if (process.env.NODE_ENV === 'production') {
+  publicAccessDocuments = require('./../../docs/public_access_docs.json')
+} else {
+  publicAccessDocuments = require('./../docs/public_access_docs.json')
+}
+
 const io = new Server(server, {
   cors: {
     origin: ['http://localhost:3000', '*', String(process.env.FRONTEND_HOST)]
@@ -28,6 +39,13 @@ app.use(cors({
 app.use(express.json())
 
 app.use(routes)
+
+// docs
+app.use('/public/docs', swaggerUI.serve, swaggerUI.setup(publicAccessDocuments))
+
+// celebrate errors
+app.use(errors())
+
 
 // run routines
 routines(io)
