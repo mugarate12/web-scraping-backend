90dc710c011f0f60bd5880a4460cf5412aeb8a80
Mateus Cardoso <serjumano17@gmail.com>
2021-12-14 14:01:48 -0300

    refactor!: close tmp files in single update

--- src/controllers/downDetector.ts
+++ src/controllers/downDetector.ts
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer'
 import moment from 'moment'
 import { Server } from 'socket.io'
 import axios from 'axios'
+import fs from 'fs'
 
 import { downDetectorData } from './../interfaces/downDetector'
 import {
@@ -26,6 +27,74 @@ export default class DownDetectorController {
 
   private sleep = (seconds: number) => {
     return new Promise(resolve => setTimeout(resolve, 1000 * seconds))
+  }
+
+  private runBrowser = async () => {
+    const minimal_args = [
+      '--incognito',
+  
+      '--autoplay-policy=user-gesture-required',
+      '--disable-background-networking',
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-breakpad',
+      '--disable-client-side-phishing-detection',
+      '--disable-component-update',
+      '--disable-default-apps',
+      '--disable-dev-shm-usage',
+      '--disable-domain-reliability',
+      '--disable-extensions',
+      '--disable-features=AudioServiceOutOfProcess',
+      '--disable-hang-monitor',
+      '--disable-ipc-flooding-protection',
+      '--disable-notifications',
+      '--disable-offer-store-unmasked-wallet-cards',
+      '--disable-popup-blocking',
+      '--disable-print-preview',
+      '--disable-prompt-on-repost',
+      '--disable-renderer-backgrounding',
+      '--disable-setuid-sandbox',
+      '--disable-speech-api',
+      '--disable-sync',
+      '--hide-scrollbars',
+      '--ignore-gpu-blacklist',
+      '--metrics-recording-only',
+      '--mute-audio',
+      '--no-default-browser-check',
+      '--no-first-run',
+      '--no-pings',
+      '--no-sandbox',
+      '--no-zygote',
+      '--password-store=basic',
+      '--use-gl=swiftshader',
+      '--use-mock-keychain',
+    ]
+  
+    const browser = await puppeteer.launch({ 
+      headless: true, 
+      args: minimal_args,
+      // userDataDir: false
+    })
+    
+    return browser
+  }
+
+  private closeBrowser = async (browser: puppeteer.Browser) => {
+    let chromeTmpDataDir: string = ''
+
+    let chromeSpawnArgs = browser.process()?.spawnargs
+
+    if (!!chromeSpawnArgs) {
+      for (let i = 0; i < chromeSpawnArgs.length; i++) {
+        if (chromeSpawnArgs[i].indexOf("--user-data-dir=") === 0) {
+            chromeTmpDataDir = chromeSpawnArgs[i].replace("--user-data-dir=", "");
+        }
+      }
+    }
+
+    await browser.close()
+
+    fs.rmSync(chromeTmpDataDir, { recursive: true, force: true })
   }
 
   private requestToSolveCaptcha = async (sitekey: string, pageURL: string) => {
@@ -388,11 +457,7 @@ export default class DownDetectorController {
   public accessDownDetectorSingleUpdate = async (req: Request, res: Response) => {
     const { serviceName } = req.params
 
-    const browser = await puppeteer.launch({ 
-      headless: true, 
-      args: ['--no-sandbox'], 
-      slowMo: 200
-    })
+    const browser = await this.runBrowser()
 
     const page = await browser.newPage()
 
@@ -463,7 +528,7 @@ export default class DownDetectorController {
     await this.updateChangeHistory(result)
     await Promise.all(registryDataPromises)
 
-    await browser.close()
+    await this.closeBrowser(browser)
 
     return res.status(200).json({
       message: 'serviço atualizado com sucesso!'
