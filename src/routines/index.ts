633f0c6f13834705a962be2a1292bb7554f89ffe
Mateus Cardoso <serjumano17@gmail.com>
2021-12-14 12:57:49 -0300

    refactor!: added logs to clean routine

--- src/routines/index.ts
+++ src/routines/index.ts
@@ -92,10 +92,13 @@ function cleanTemporaryFiles() {
 
   if (isLinux) {
     let filesList = fs.readdirSync('/tmp')
+    console.log(filesList)
     
     filesList.forEach((file) => {
       const isTemporaryFileOfPuppeteer = file.includes('puppeteer_dev_chrome_profile-')
       const isTemporaryFileOfChromium = file.includes('.org.chromium.Chromium.')
+
+      console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
 
       if (isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium) {
         fs.rmSync(file, { recursive: true, force: true })
