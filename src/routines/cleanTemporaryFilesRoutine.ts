d20681fa12969651b75474dcd05c442b0bf5b2f0
Mateus Cardoso <serjumano17@gmail.com>
2022-01-31 14:56:36 -0300

    refactor!: clear temporary files routines

--- src/routines/cleanTemporaryFilesRoutine.ts
+++ src/routines/cleanTemporaryFilesRoutine.ts
@@ -1,26 +1,52 @@
 import CronJob from 'cron'
 import fs from 'fs'
 
+import { exec } from 'child_process'
+import path from 'path'
+
 export function cleanTemporaryFiles() {
   const isLinux = process.platform === 'darwin' || process.platform === 'linux'
 
   if (isLinux) {
     let filesList = fs.readdirSync('/tmp')
-    // console.log(filesList)
-    
+
     filesList.forEach((file) => {
       const isTemporaryFileOfPuppeteer = file.includes('puppeteer_dev_chrome_profile-')
       const isTemporaryFileOfChromium = file.includes('.org.chromium.Chromium.')
 
-      // console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
+      console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
 
-      if (isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium) {
-        fs.rmSync(file, { recursive: true, force: true })
-      }
+      exec(``, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`
+            TEMPORARY FILES --> Erro ao excluir arquivos temporários
+          `)
+        }
+
+        if (stderr) {
+          console.log(`
+            TEMPORARY FILES --> Erro ao excluir arquivos temporários
+          `)
+        }
+      })
     })
 
+    // let filesList = fs.readdirSync('/tmp')
+    // // console.log(filesList)
+    
+    // filesList.forEach((file) => {
+    //   const isTemporaryFileOfPuppeteer = file.includes('puppeteer_dev_chrome_profile-')
+    //   const isTemporaryFileOfChromium = file.includes('.org.chromium.Chromium.')
+
+    //   console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
+
+    //   if (isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium) {
+    //     fs.rmSync(file, { recursive: true, force: true })
+    //   }
+    // })
+
     console.log(`
-      TEMPORARY FILES --> arquivos temporários excluídos com sucesso!
+      TEMPORARY FILES --> rotina de exclusão de arquivos temporários concluída !
     `)
   }
 }
