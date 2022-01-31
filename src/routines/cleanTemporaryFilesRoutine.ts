972c2702dc05a9a1eefbc6b3a5552dea68998097
Mateus Cardoso <serjumano17@gmail.com>
2022-01-31 15:18:04 -0300

    fix!: clear routine

--- src/routines/cleanTemporaryFilesRoutine.ts
+++ src/routines/cleanTemporaryFilesRoutine.ts
@@ -8,44 +8,44 @@ export function cleanTemporaryFiles() {
   const isLinux = process.platform === 'darwin' || process.platform === 'linux'
 
   if (isLinux) {
-    let filesList = fs.readdirSync('/tmp')
-
-    filesList.forEach((file) => {
-      const isTemporaryFileOfPuppeteer = file.includes('puppeteer_dev_chrome_profile-')
-      const isTemporaryFileOfChromium = file.includes('.org.chromium.Chromium.')
-
-      console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
-
-      exec(`rm -r ${file}`, (error, stdout, stderr) => {
-        if (error) {
-          console.log(`
-            TEMPORARY FILES --> Erro ao excluir arquivos temporários
-          `)
-
-          return
-        }
-
-        if (stderr) {
-          console.log(`
-            TEMPORARY FILES --> Erro ao excluir arquivos temporários
-          `)
-        }
-      })
-    })
-
     // let filesList = fs.readdirSync('/tmp')
-    // // console.log(filesList)
-    
+
     // filesList.forEach((file) => {
     //   const isTemporaryFileOfPuppeteer = file.includes('puppeteer_dev_chrome_profile-')
     //   const isTemporaryFileOfChromium = file.includes('.org.chromium.Chromium.')
 
     //   console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
 
-    //   if (isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium) {
-    //     fs.rmSync(file, { recursive: true, force: true })
-    //   }
+    //   exec(`rm -r ${file}`, (error, stdout, stderr) => {
+    //     if (error) {
+    //       console.log(`
+    //         TEMPORARY FILES --> Erro ao excluir arquivos temporários
+    //       `)
+
+    //       return
+    //     }
+
+    //     if (stderr) {
+    //       console.log(`
+    //         TEMPORARY FILES --> Erro ao excluir arquivos temporários
+    //       `)
+    //     }
+    //   })
     // })
+
+    let filesList = fs.readdirSync('/tmp')
+    // console.log(filesList)
+    
+    filesList.forEach((file) => {
+      const isTemporaryFileOfPuppeteer = file.includes('puppeteer_dev_chrome_profile-')
+      const isTemporaryFileOfChromium = file.includes('.org.chromium.Chromium.')
+
+      console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
+
+      if (isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium) {
+        fs.rmSync(file, { recursive: true, force: true })
+      }
+    })
 
     console.log(`
       TEMPORARY FILES --> rotina de exclusão de arquivos temporários concluída !
