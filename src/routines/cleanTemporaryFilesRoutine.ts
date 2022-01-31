745f02d695e58153f0256eafd5afb55d0f945707
Mateus Cardoso <serjumano17@gmail.com>
2022-01-31 15:29:06 -0300

    fix and solved: clear routine of temporary files

--- src/routines/cleanTemporaryFilesRoutine.ts
+++ src/routines/cleanTemporaryFilesRoutine.ts
@@ -1,38 +1,10 @@
 import CronJob from 'cron'
 import fs from 'fs'
 
-import { exec } from 'child_process'
-import path from 'path'
-
 export function cleanTemporaryFiles() {
   const isLinux = process.platform === 'darwin' || process.platform === 'linux'
 
   if (isLinux) {
-    // let filesList = fs.readdirSync('/tmp')
-
-    // filesList.forEach((file) => {
-    //   const isTemporaryFileOfPuppeteer = file.includes('puppeteer_dev_chrome_profile-')
-    //   const isTemporaryFileOfChromium = file.includes('.org.chromium.Chromium.')
-
-    //   console.log('if: ', isTemporaryFileOfPuppeteer || isTemporaryFileOfChromium, 'file: ', file)
-
-    //   exec(`rm -r ${file}`, (error, stdout, stderr) => {
-    //     if (error) {
-    //       console.log(`
-    //         TEMPORARY FILES --> Erro ao excluir arquivos temporários
-    //       `)
-
-    //       return
-    //     }
-
-    //     if (stderr) {
-    //       console.log(`
-    //         TEMPORARY FILES --> Erro ao excluir arquivos temporários
-    //       `)
-    //     }
-    //   })
-    // })
-
     let filesList = fs.readdirSync('/tmp')
     // console.log(filesList)
     
