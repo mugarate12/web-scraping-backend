f4961bcd1571cb47f3ca300e3a5e63ecd06a7baa
Mateus Cardoso <serjumano17@gmail.com>
2022-02-18 08:50:41 -0300

    refactor!: add validation to cliente able in public access jwt

--- src/middlewares/publicAccessJWT.ts
+++ src/middlewares/publicAccessJWT.ts
@@ -2,6 +2,10 @@ import jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express'
 import dotenv from 'dotenv'
 
+import {
+  apiAccessClientsRepository
+} from './../repositories'
+
 import { AppError, errorHandler } from './../utils/handleError'
 
 dotenv.config()
@@ -12,6 +16,8 @@ interface decodedTokenInterface {
   id: string;
 }
 
+// 'cliente está desabilitado, por favor, entre em contato com o administrador do sistema'
+
 export default async function publicAccessJWT(req: Request, res: Response, next: NextFunction) {
   const authToken = req.query.token
 
@@ -20,11 +26,35 @@ export default async function publicAccessJWT(req: Request, res: Response, next:
   }
 
   try {
-    return await jwt.verify(authToken, JWT_SECRET, (error, decoded) => {
+    return await jwt.verify(authToken, JWT_SECRET, async (error, decoded) => {
       if (error) {
         return errorHandler(new AppError('Authorization Error', 401, 'Não autorizado ou token inválido', true), res)
       }
       
+      let haveAccess = await apiAccessClientsRepository.get({
+        id: Number(decoded?.id)
+      })
+        .then(response => {
+          if (response.able === 2) {
+            // res.status(406).json({
+            //   message: 'cliente está desabilitado, por favor, entre em contato com o administrador do sistema'
+            // })
+  
+            return false
+          } else {
+            return true
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        
+          return false
+        })
+
+      if (!haveAccess) {
+        return errorHandler(new AppError('Authorization Error', 406, 'cliente está desabilitado ou não existe, por favor, entre em contato com o administrador do sistema', true), res)
+      }
+
       res.setHeader('userID', decoded?.id)
       return next()
     })
